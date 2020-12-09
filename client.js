2ef1bda94d9a77d9749ac00d652bc54108aab2d2
tnfssc <tnfssc@gmail.com>
2020-12-10 01:02:59 +0530

    Client exit 1 when arguments too low

--- client.js
+++ client.js
@@ -1,19 +1,23 @@
 const tls = require("tls");
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
+if (args.length < 1) process.exit(1);
 const PORT = args[1] || process.env.PORT;
 const HOST = args[0] || process.env.HOST;
 
-var client = tls.connect({ rejectUnauthorized: false, host: HOST.split(':')[0], port: HOST.split(':')[1] || PORT}, () => {
-  if (client.authorized) {
-    console.log("Connection authorized by a Certificate Authority.");
-  } else {
-    console.log("Connection not authorized: " + client.authorizationError);
+var client = tls.connect(
+  { rejectUnauthorized: false, host: HOST.split(":")[0], port: HOST.split(":")[1] || PORT },
+  () => {
+    if (client.authorized) {
+      console.log("Connection authorized by a Certificate Authority.");
+    } else {
+      console.log("Connection not authorized: " + client.authorizationError);
+    }
+    client.write("I am the client sending you a message.");
   }
-  client.write("I am the client sending you a message.");
-});
+);
 
-client.on("data", (data) => {
+client.on("data", data => {
   console.log("Received: %s [it is %d bytes long]", data.toString().replace(/(\n)/gm, ""), data.length);
   client.end();
 });
@@ -22,7 +26,7 @@ client.on("close", () => {
   console.log("Connection closed");
 });
 
-client.on("error", (error) => {
+client.on("error", error => {
   console.error(error);
   client.destroy();
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const tls = require("tls");
 const fs = require("fs");
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 const PORT = parseInt(args[0]) || process.env.PORT || 1337;
 
 const options = {
@@ -10,7 +10,7 @@ const options = {
   rejectUnauthorized: false,
 };
 
-const server = tls.createServer(options, (socket) => {
+const server = tls.createServer(options, socket => {
   socket.write("I am the server sending you a message.");
   socket.on("data", function (data) {
     console.log("Received: %s [it is %d bytes long]", data.toString().replace(/(\n)/gm, ""), data.length);
@@ -24,7 +24,7 @@ server.listen(PORT, () => {
   console.log("I'm listening on port %d", PORT);
 });
 
-server.on("error", (error) => {
+server.on("error", error => {
   console.error(error);
   server.destroy();
 });
