de20324a4f5a4976b109d497525d525c348589fc
tnfssc <tnfssc@gmail.com>
2020-12-10 00:26:09 +0530

    Trimmed code

--- client.js
+++ client.js
@@ -1,43 +1,28 @@
-"use strict";
-
-var tls = require("tls");
+const tls = require("tls");
 
 const args = process.argv.slice(2)
 const PORT = args[1] || process.env.PORT || 1337;
 const HOST = args[0] || process.env.HOST || "127.0.0.1";
-console.log(process.argv)
-// Pass the certs to the server and let it know to process even unauthorized certs.
-var options = {
-  rejectUnauthorized: false,
-};
 
 var client = tls.connect(PORT, HOST, { rejectUnauthorized: false }, () => {
-  // Check if the authorization worked
   if (client.authorized) {
     console.log("Connection authorized by a Certificate Authority.");
   } else {
     console.log("Connection not authorized: " + client.authorizationError);
   }
-
-  // Send a friendly message
   client.write("I am the client sending you a message.");
 });
 
-client.on("data", function (data) {
+client.on("data", (data) => {
   console.log("Received: %s [it is %d bytes long]", data.toString().replace(/(\n)/gm, ""), data.length);
-
-  // Close the connection after receiving the message
   client.end();
 });
 
-client.on("close", function () {
+client.on("close", () => {
   console.log("Connection closed");
 });
 
-// When an error ocoures, show it.
-client.on("error", function (error) {
+client.on("error", (error) => {
   console.error(error);
-
-  // Close the connection after the error occurred.
   client.destroy();
 });
--- server.js
+++ server.js
@@ -1,41 +1,30 @@
-"use strict";
-
-var tls = require("tls");
-var fs = require("fs");
+const tls = require("tls");
+const fs = require("fs");
 
 const args = process.argv.slice(2)
 const PORT = parseInt(args[1]) || process.env.PORT || 1337;
 
-var options = {
+const options = {
   key: fs.readFileSync("./certs/key.pem"),
   cert: fs.readFileSync("./certs/cert.pem"),
   rejectUnauthorized: false,
 };
 
-var server = tls.createServer(options, function (socket) {
-  // Send a friendly message
+const server = tls.createServer(options, (socket) => {
   socket.write("I am the server sending you a message.");
-
-  // Print the data that we received
   socket.on("data", function (data) {
     console.log("Received: %s [it is %d bytes long]", data.toString().replace(/(\n)/gm, ""), data.length);
   });
-
-  // Let us know when the transmission is over
-  socket.on("end", function () {
+  socket.on("end", () => {
     console.log("EOT (End Of Transmission)");
   });
 });
 
-// Start listening on a specific port and address
-server.listen(PORT, function () {
+server.listen(PORT, () => {
   console.log("I'm listening on port %d", PORT);
 });
 
-// When an error occurs, show it.
-server.on("error", function (error) {
+server.on("error", (error) => {
   console.error(error);
-
-  // Close the connection after the error occurred.
   server.destroy();
 });
