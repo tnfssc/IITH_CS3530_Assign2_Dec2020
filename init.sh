1aa4d3f5cc5265ce6345983d0ceea158bbe754e5
tnfssc <tnfssc@gmail.com>
2020-12-09 20:16:50 +0530

    Script for certs created

---
+++ init.sh
@@ -0,0 +1,6 @@
+mkdir -p certs 
+cd certs 
+openssl genrsa -out key.pem 
+openssl req -new -key key.pem -subj \"/C=IN/ST=CA/O=Cool/CN=localhost\" -out csr.pem 
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem 
+cd ..
